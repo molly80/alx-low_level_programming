1e51408c7028340b6159e77132236433065bfa68
molly80 <maureenwangui09@gmail.com>
2022-04-20 18:49:04 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap_int *a, *b
+ * @a: pointer 1
+ * @b: pointer 2
+ * pointer a gets the value of b while pointer b gets the value of a
+ * Return void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
